aa0d9e63e4800f329abcc42612ab23f05649a1db
mwas <francismwangi152@gmail.com>
2015-06-23 17:03:07 +0300

    added the class for Graph

--- graph.py
+++ graph.py
@@ -1,1 +1,13 @@
 __author__ = 'francis'
+
+class Graph(object):
+
+    def __init__(self, initial_node):
+
+        self.graph_dict = {}
+
+        # add the first node
+        self.graph_dict.update({ initial_node: {}})
+
+    def add_node(self, parent, object, **kwargs):
+        pass
